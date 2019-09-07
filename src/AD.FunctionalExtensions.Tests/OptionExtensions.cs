f4d43fd8f72cbdba72066cbaef047a852d24d575
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2019-09-07 14:39:05 +0200

    Match

---
+++ src/AD.FunctionalExtensions.Tests/OptionExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace AD.FunctionalExtensions.Tests
+{
+    [TestClass]
+    public class OptionExtensions
+    {
+        static readonly Random rnd = new Random();
+
+        [TestMethod]
+        public void Some()
+        {
+            var value = rnd.Next();
+            var a = Option.Some(value);
+            var b = value.Some();
+
+            AreEqual(a, b);
+        }
+
+        [TestMethod]
+        public void Match_IsSome()
+        {
+            var value = rnd.Next();
+            var option = value.Some();
+
+            var matchedValue = rnd.Next();
+            
+            var actual =
+                option.Match(
+                    onIsSome: v =>
+                    {
+                        AreEqual(value, v);
+                        return matchedValue;
+                    },
+                    onIsNone: () => throw new AssertFailedException("'onIsSome' expected"));
+
+            AreEqual(matchedValue, actual);
+        }
+
+        [TestMethod]
+        public void Match_IsNone()
+        {
+            var option = Option<int>.None;
+
+            var matchedValue = rnd.Next();
+
+            var actual =
+                option.Match(
+                    onIsSome: _ => throw new AssertFailedException("'onIsNone' expected"),
+                    onIsNone: () => matchedValue);
+
+            AreEqual(matchedValue, actual);
+        }
+    }
+}
---
+++ src/AD.FunctionalExtensions/OptionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AD.FunctionalExtensions
+{
+    public static class OptionExtensions
+    {
+        public static Option<T> Some<T>(this T value) => Option.Some(value);
+
+        public static U Match<T, U>(this Option<T> option, Func<T, U> onIsSome, Func<U> onIsNone)
+        {
+            if (onIsSome is null) throw new ArgumentNullException(nameof(onIsSome));
+            if (onIsNone is null) throw new ArgumentNullException(nameof(onIsNone));
+
+            return option.IsSome(out var value) ? onIsSome(value) : onIsNone();
+        }
+    }
+}
