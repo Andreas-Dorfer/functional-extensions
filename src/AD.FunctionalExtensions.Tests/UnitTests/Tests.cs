0ad7815bddc5dfc6db3deafc1103f5a0062f691d
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-07-11 12:53:16 +0200

    Unit type

---
+++ src/AD.FunctionalExtensions.Tests/UnitTests/Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AD.FunctionalExtensions.Tests.UnitTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void All_Units_are_equal()
+        {
+            Unit a = default;
+            Unit b = new();
+            Unit c = Unit.Value;
+
+            AssertAreEqual(a, b);
+            AssertAreEqual(b, c);
+        }
+
+        static void AssertAreEqual(Unit a, Unit b)
+        {
+            Assert.AreEqual(a.GetHashCode(), b.GetHashCode());
+            Assert.AreEqual(a, b);
+            Assert.IsTrue(a == b);
+            Assert.IsFalse(a != b);
+            Assert.AreEqual(a.ToString(), b.ToString());
+        }
+    }
+}
---
+++ src/AD.FunctionalExtensions/Unit.cs
@@ -0,0 +1,25 @@
+﻿namespace AD.FunctionalExtensions
+{
+    public readonly struct Unit
+    {
+        static readonly Unit value = default;
+
+        public static ref readonly Unit Value => ref value;
+
+        public override int GetHashCode() => 0;
+
+        public override bool Equals(object? obj) =>
+            obj switch
+            {
+                Unit => true,
+                _ => false
+            };
+
+        public override string ToString() => "()";
+
+#pragma warning disable IDE0060 // Remove unused parameter
+        public static bool operator ==(in Unit a, in Unit b) => true;
+        public static bool operator !=(in Unit a, in Unit b) => false;
+#pragma warning restore IDE0060 // Remove unused parameter
+    }
+}
